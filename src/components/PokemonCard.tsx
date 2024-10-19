20289c06aedb0cf441dd03d881e61de3194f9351
blksy <79380937+blksy@users.noreply.github.com>
2024-10-19 23:45:28 +0200

    .

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+import style from "./PokemonCard.module.css";
+interface PokemonCardProps {
+  name: string;
+  imageUrl: string;
+  types: string[];
+}
+export default function PokemonCard{
+  return (
 
-export default function PokemonCard() {
-  return <div></div>;
+  );
 }
