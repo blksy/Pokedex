5d3fc8666945fc9025229d65f31a444348b5d877
blksy <79380937+blksy@users.noreply.github.com>
2024-10-23 22:58:04 +0200

    used destructuring to reduce the amount of code

--- src/api/PokemonApi.tsx
+++ src/api/PokemonApi.tsx
@@ -3,25 +3,21 @@ import axios from "axios";
 const API_BASE_URL = "https://pokeapi.co/api/v2/";
 
 export const fetchPokemon = async (offset: number, limit: number) => {
-  const response = await axios.get(
+  const { data } = await axios.get(
     `${API_BASE_URL}pokemon?offset=${offset}&limit=${limit}`
   );
-  return response.data.results;
+  return data.results;
 };
 export const fetchPokemonDetails = async (pokemonName: string) => {
   try {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
-    );
+    const { data } = await axios.get(`${API_BASE_URL}pokemon/${pokemonName}`);
     return {
-      name: response.data.name,
-      sprite: response.data.sprites.other["official-artwork"].front_default,
-      types: response.data.types.map((type: any) => type.type.name),
-      height: response.data.height,
-      weight: response.data.weight,
-      abilities: response.data.abilities.map(
-        (ability: any) => ability.ability.name
-      ),
+      name: data.name,
+      sprite: data.sprites.other["official-artwork"].front_default,
+      types: data.types.map((type: any) => type.type.name),
+      height: data.height,
+      weight: data.weight,
+      abilities: data.abilities.map((ability: any) => ability.ability.name),
     };
   } catch (error) {
     console.error("Error fetching Pokémon details:", error);
